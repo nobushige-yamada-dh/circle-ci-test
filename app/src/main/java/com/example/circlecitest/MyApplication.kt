f1ab5254f2be8caab76288e9dece3a2cc24baabb
YAMADA Nobushige <nobushige.yamada@digitalhearts.com>
2019-09-16 19:46:23 +0900

    test

--- app/src/main/java/com/example/circlecitest/MyApplication.kt
+++ app/src/main/java/com/example/circlecitest/MyApplication.kt
@@ -19,4 +19,29 @@ class MyApplication : Application() {
         appComponent = DaggerAppComponent.builder().application(this)
             .build()
     }
+
+    fun forCyclomaticComplexity(a: Int): Int {
+        if (a == 1) {
+            return 1
+        }
+        if (a == 2) {
+            return 2
+        }
+        if (a == 3) {
+            return 3
+        }
+        if (a == 4) {
+            return 4
+        }
+        if (a == 5) {
+            return 5
+        }
+        if (a == 6) {
+            return 6
+        }
+        if (a == 7) {
+            return 7
+        }
+        return 0
+    }
 }
