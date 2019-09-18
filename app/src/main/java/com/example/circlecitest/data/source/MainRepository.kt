a29fd4969a30c2ffbf02adeb646d660f7e639cc8
Yamada Nobushige <nobushige.yamada@digitalhearts.com>
2019-09-18 19:13:40 +0900

    comment

--- app/src/main/java/com/example/circlecitest/data/source/MainRepository.kt
+++ app/src/main/java/com/example/circlecitest/data/source/MainRepository.kt
@@ -1,7 +1,8 @@
 package com.example.circlecitest.data.source
 
 /**
- *
+ * This is an interface of main repository.
+ * When you run unit tests, you can implement mock using this interface.
  */
 interface MainRepository {
 }
--- app/src/main/java/com/example/circlecitest/data/source/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/circlecitest/data/source/MainRepositoryImpl.kt
@@ -1,5 +1,12 @@
 package com.example.circlecitest.data.source
 
+/**
+ * This is an implementation of business logic.
+ * You MUST NOT write code for I/O, database or API.
+ * These code MUST write in 'data.source.local' or 'data.source.remote', and inject here.
+ * This class (business logic) can test by Unit Test with mock easily, without Android and I/O.
+ * You MUST keep it.
+ */
 class MainRepositoryImpl: MainRepository {
 
     var name = "me"
