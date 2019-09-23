e8a135d7ce2fb293eb9828b5eaa5087450113a58
YAMADA Nobushige <nobushige.yamada@digitalhearts.com>
2019-09-24 00:01:26 +0900

    list

--- app/src/main/java/com/example/circlecitest/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/circlecitest/ui/main/MainFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
 import com.example.circlecitest.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -31,7 +30,6 @@ class MainFragment : Fragment() {
         mainGameAppAdapter = MainGameAppAdapter(this, viewModel)
         viewDataBinding.gameItems.also {
             it.setHasFixedSize(true)
-            it.layoutManager = GridLayoutManager(context!!, 3)
             it.adapter = mainGameAppAdapter
         }
     }
