09a58dc84c8b70eb08c0b11821ec9e261c38d9b2
Yamada Nobushige <nobushige.yamada@digitalhearts.com>
2019-09-24 17:23:48 +0900

    test

--- app/src/test/java/com/example/circlecitest/ui/main/MainViewModelTest.kt
+++ app/src/test/java/com/example/circlecitest/ui/main/MainViewModelTest.kt
@@ -2,6 +2,7 @@ package com.example.circlecitest.ui.main
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.example.circlecitest.data.GameApp
 import com.example.circlecitest.data.source.AppRepository
 import com.nhaarman.mockitokotlin2.*
 import org.junit.Assert.*
@@ -22,6 +23,52 @@ class MainViewModelTest {
     }
 
     @Test
+    fun testOnClickGameApp() {
+        val appRepository = mock<AppRepository>()
+        val mainViewModel = MainViewModel(appRepository)
+        val observer = mock<Observer<GameApp>>()
+        val gameApp = GameApp(1, "game name")
+        mainViewModel.launchGameApp.observeForever(observer)
+        assertEquals(null, mainViewModel.launchGameApp.value)
+        clearInvocations(observer)
+
+        mainViewModel.onClickGameApp(gameApp)
+
+        verify(observer).onChanged(check {
+            assertEquals(gameApp.id, it.id)
+            assertEquals(gameApp.name, it.name)
+        })
+    }
+
+    @Test
+    fun testOnClickAdd() {
+        val appRepository = mock<AppRepository>()
+        val mainViewModel = MainViewModel(appRepository)
+        val observer = mock<Observer<MainViewModel.LaunchScreen>>()
+        mainViewModel.launchScreen.observeForever(observer)
+        assertEquals(null, mainViewModel.launchScreen.value)
+        clearInvocations(observer)
+
+        mainViewModel.onClickAdd()
+
+        verify(observer).onChanged(MainViewModel.LaunchScreen.REGISTER)
+    }
+
+    @Test
+    fun testOnClickHelp() {
+        val appRepository = mock<AppRepository>()
+        val mainViewModel = MainViewModel(appRepository)
+        val observer = mock<Observer<MainViewModel.LaunchScreen>>()
+        mainViewModel.launchScreen.observeForever(observer)
+        assertEquals(null, mainViewModel.launchScreen.value)
+        clearInvocations(observer)
+
+        mainViewModel.onClickHelp()
+
+        verify(observer).onChanged(MainViewModel.LaunchScreen.HELP)
+    }
+
+    @Test
     fun testOnClickFab() {
         val appRepository = mock<AppRepository>()
         val mainViewModel = MainViewModel(appRepository)
@@ -29,7 +76,9 @@ class MainViewModelTest {
         mainViewModel.snackBarMessage.observeForever(observer)
         assertEquals(null, mainViewModel.snackBarMessage.value)
         clearInvocations(observer)
+
         mainViewModel.onClickFab()
+
         verify(observer).onChanged(MainViewModel.Message.FAB_PUSHED)
     }
 }
